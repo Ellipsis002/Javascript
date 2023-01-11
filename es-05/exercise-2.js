a9845a10239e00110b8bf2ebc6ee021352cb9ade
Francesco Cricchio <cricchio.francesco94@gmail.com>
2023-01-11 14:17:05 +0100

    forse fine esercizio

---
+++ es-05/exercise-2.js
@@ -0,0 +1,39 @@
+let ceo = 'ceo';
+let manager = 'manager';
+let cto = 'cto';
+let developer = 'developer';
+
+
+function calculateSalary(role) {
+
+
+  switch(role) {
+    case ceo:
+      salary=2200;
+      break;
+    case manager:
+      salary=1800
+      break;
+    case cto:
+      salary=1800;
+      break;
+    case developer:
+      salary=1500;
+      break;
+    default:
+      salary=1000
+    }
+    return salary
+}
+
+const ceoSalary = calculateSalary('ceo');
+const managerSalary = calculateSalary('manager');
+const ctoSalary = calculateSalary('cto');
+const developerSalary = calculateSalary('developer');
+const otherSalary = calculateSalary('other');
+
+console.log(ceoSalary);
+console.log(managerSalary);
+console.log(ctoSalary);
+console.log(developerSalary);
+console.log(otherSalary);
