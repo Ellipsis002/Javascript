48a4fa12c48ce90260932bf95e0b4a776b989c63
Francesco Cricchio <cricchio.francesco94@gmail.com>
2023-01-15 13:36:12 +0100

    fine esercizio

---
+++ es-23/exercise-1.js
@@ -0,0 +1,12 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+// Print values of person using Object.keys
+
+let profile = Object.keys (person);
+let profileValues = Object.values (person);
+console.log(profile);
+console.log(profileValues);
